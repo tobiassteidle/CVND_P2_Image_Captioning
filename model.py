0a7e75d1c9b26bc07844df169ea971596e4b95e8
Tobias Steidle <tobias.steidle@softwaredev.de>
2020-01-25 17:45:45 +0100

    feat: added solution

--- model.py
+++ model.py
@@ -23,11 +23,34 @@ class EncoderCNN(nn.Module):
 
 class DecoderRNN(nn.Module):
     def __init__(self, embed_size, hidden_size, vocab_size, num_layers=1):
-        pass
+        super(DecoderRNN, self).__init__()
+        
+        self.embeddings = nn.Embedding(vocab_size, embed_size)
+        self.lstm = nn.LSTM(embed_size, hidden_size, num_layers, batch_first=True)
+        self.linear = nn.Linear(hidden_size, vocab_size)
     
     def forward(self, features, captions):
-        pass
+        captions = captions[:, :-1]
+        
+        embedding = self.embeddings(captions)        
+        embedding = torch.cat((features.unsqueeze(dim=1), embedding), dim=1)
+        lstm_out, hidden = self.lstm(embedding)
+        
+        outputs = self.linear(lstm_out)        
+        return outputs
 
     def sample(self, inputs, states=None, max_len=20):
-        " accepts pre-processed image tensor (inputs) and returns predicted sentence (list of tensor ids of length max_len) "
-        pass
+        predicted_sentence = []
+        
+        for i in range(max_len):            
+            lstm_out, states = self.lstm(inputs, states)
+            lstm_out = lstm_out.squeeze(1)
+            
+            outputs = self.linear(lstm_out)
+            
+            predicted = outputs.max(1)[1]                                                 
+            predicted_sentence.append(predicted.item())
+
+            inputs = self.embeddings(predicted).unsqueeze(1)
+            
+        return predicted_sentence
